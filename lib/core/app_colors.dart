a2acb347ba6ca0256b829a7c0d9f12bae68f3064
jeni63 <jeniferkue63@gmail.com>
2022-05-22 23:13:37 +0200

    first commit

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF0C9869);
+const Color kSecondaryColor = Color(0xFF3C4046);
+const Color kBackgroundColor = Color(0xFFF9F8FD);
---
+++ lib/core/app_size.dart
@@ -0,0 +1,8 @@
+
+
+const double kDefaultPadding = 20.0;
+const double kDefaultMargin = 10.0;
+const double kDefaultRadius = 10.0;
+const double kSizedBoxHeight = 16.0;
+const double kSizedBoxWidth = 10.0;
+
